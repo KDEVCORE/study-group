3c6cbb70a098bf4f355de6975ded4a6a0591bc9a
KDEVCORE <kdevcore@gmail.com>
2023-02-21 13:59:18 +0900

    challenge failed, counterexample not found

--- common challenges/2xN pretty tiling (18230)/src/Main.java
+++ common challenges/2xN pretty tiling (18230)/src/Main.java
@@ -19,24 +19,19 @@ public class Main {
         for(int i=0; i<typeB; i++) tilesB.offer(Integer.parseInt(st.nextToken()));
 
         long result = Integer.MIN_VALUE, sum = 0;
-        int temp = size > 2*typeB ? typeB : size / 2, flag = 0;
+        int flag = size > 2*typeB ? typeB : size / 2;
         PriorityQueue<Integer> used = new PriorityQueue<>();
-        while(!tilesB.isEmpty() && flag < temp) { // B타일 합계
-            int pretty = tilesB.poll();
-            used.offer(pretty);
-            sum += pretty;
-            flag++;
+        for(int i=1; i<=flag; i++) { // 사용할 수 있는 B타일 합계
+            int temp = tilesB.poll();
+            used.offer(temp);
+            sum += temp;
         }
-        flag = 0;
-        while(!tilesA.isEmpty() && flag < size-2*temp) {
+        for(int i=1; i<=size-2*flag; i++) {
             sum += tilesA.poll(); // A타일 합계
-            flag++;
         }
         result = Math.max(result, sum); // 타일 모두 채웠을 때 합계
         
-        if(tilesA.size() < 2) {
-            System.out.println(result);
-        } else {
+        if(tilesA.size() > 1) { // 잔여 A타일이 2개 이상일 경우, 기존 B타일 1개와 비교 로직
             for(int item: used) { // 사용했던 B타일
                 while(!tilesA.isEmpty()) { // 최댓값 검증
                     int tile1 = tilesA.poll();
@@ -44,12 +39,14 @@ public class Main {
                     if(tile1+tile2 > item) {
                         sum = (sum - item) + (tile1 + tile2);
                         result = Math.max(result, sum); // 타일 모두 채웠을 때 합계
-                    } else {
+                    } else { // A타일 2개(큰 값)와 B타일 1개(작은 값) 비교 성립이 안 되는 경우, 종료
                         System.out.println(result);
                         return;
                     }
                 }
+                if(tilesA.isEmpty()) break; // A타일이 없을 경우, 사용했던 타일 비교 종료
             }
         }
+        System.out.println(result);
     }
 }
