40d046eead26b6cf22ff943cdb7590042fe2911c
KDEVCORE <kdevcore@gmail.com>
2023-02-21 18:31:20 +0900

    challenge success

--- common challenges/2xN pretty tiling (18230)/src/Main.java
+++ common challenges/2xN pretty tiling (18230)/src/Main.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
@@ -18,33 +19,26 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<typeB; i++) tilesB.offer(Integer.parseInt(st.nextToken()));
 
-        long result = Integer.MIN_VALUE, sum = 0;
+        long sum = 0;
         int stack = 0;
-        PriorityQueue<Integer> used = new PriorityQueue<>();
-        while(!tilesB.isEmpty()) { // 사용할 수 있는 B타일 합계
-            int temp = tilesB.poll();
-            sum += temp;
-            used.offer(temp);
+        ArrayList<Integer> resultB = new ArrayList<>();
+        while(!tilesB.isEmpty() && size - stack > 1) {
+            resultB.add(tilesB.poll());
             stack += 2;
-            if(size - stack < 2) break;
         }
+        Collections.sort(resultB);
         while(!tilesA.isEmpty() && size - stack > 0) {
             sum += tilesA.poll();
             stack += 1;
         }
-        result = Math.max(result, sum); // 타일 모두 채웠을 때 합계
-        
-        if(used.size() > 0) {
-            for(int item: used) {
-                if(tilesA.size() < 2) break;
-                int tile1 = tilesA.poll();
-                int tile2 = tilesA.poll();
-                if(item < tile1 + tile2) {
-                    sum = (sum - item) + (tile1 + tile2);
-                    result = Math.max(result, sum);
-                } else break;
+        for(int item: resultB) {
+            int temp = 0;
+            if(tilesA.size() > 1) {
+                for(int i=0; i<2; i++) temp += tilesA.poll();
+                if(temp> item) item = temp;
             }
+            sum += item;
         }
-        System.out.println(result);
+        System.out.println(sum);
     }
 }
