f630871d023b47eb3c7e264efabe03ec808e15e4
KDEVCORE <kdevcore@gmail.com>
2023-02-21 16:50:54 +0900

    challenge success

--- common challenges/four players (2121)/src/Main.java
+++ common challenges/four players (2121)/src/Main.java
@@ -1,66 +1,54 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int dotsCount = Integer.parseInt(br.readLine());
+        int dotCount = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         int width = Integer.parseInt(st.nextToken());
         int length = Integer.parseInt(st.nextToken());
-        ArrayList<Dots> coordinates = new ArrayList<>();
-        for(int i=0; i<dotsCount; i++) {
+        ArrayList<Dot> dots = new ArrayList<>();
+        for(int i=0; i<dotCount; i++) {
             st = new StringTokenizer(br.readLine());
-            coordinates.add(new Dots(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+            dots.add(new Dot(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
         }
+        Collections.sort(dots);
         int count = 0;
-        int[][] dx = {
-            {0, width, 0, -width},
-            {0, width, 0, -width},
-            {0, -width, 0, width},
-            {0, -width, 0, width}
-        };
-        int[][] dy = {
-            {length, 0, -length, 0},
-            {-length, 0, length, 0},
-            {length, 0, -length, 0},
-            {-length, 0, length, 0}
-        };
-        for(Dots item: coordinates) {
-            boolean valid = false;
-            int fx = item.x, fy = item.y;
-            for(int i=0; i<4; i++) {
-                for(int j=0; j<4; j++) {
-                    fx += dx[i][j];
-                    fy += dy[i][j];
-                    if(!coordinates.contains(new Dots(fx, fy))) {
-                        valid = false;
-                        break;
-                    } else {
-                        valid = true;
-                    }
-                }
-                if(valid && item.x == fx && item.y == fy) count++;
-            }
+        for(Dot item: dots) {
+            Dot dot1 = new Dot(item.x+width, item.y);
+            Dot dot2 = new Dot(item.x+width, item.y+length);
+            Dot dot3 = new Dot(item.x, item.y+length);
+            if(bsDot(dots, dot1) && bsDot(dots, dot2) && bsDot(dots, dot3)) count++;
         }
         System.out.println(count);
     }
+    private static boolean bsDot(ArrayList<Dot> dots, Dot target) {
+        int min = 0, max = dots.size() - 1;
+        while(min <= max) {
+            int mid = (min + max) / 2;
+            if(dots.get(mid).x == target.x && dots.get(mid).y == target.y) return true;
+            else {
+                if(target.x < dots.get(mid).x || (target.x == dots.get(mid).x && target.y < dots.get(mid).y)) max = mid - 1;
+                else min = mid + 1;
+            }
+        }
+        return false;
+    }
 }
-class Dots {
+class Dot implements Comparable<Dot> {
     int x, y;
-    public Dots(int x, int y) {
+    public Dot(int x, int y) {
         this.x = x;
         this.y = y;
     }
+
     @Override
-    public boolean equals(Object object) {
-        Dots dot = (Dots) object;
- 
-        if (this.x == dot.x && this.y == dot.y) {
-            return true;
-        }
-        return false;
+    public int compareTo(Dot dot) {
+        if(this.x == dot.x) return this.y - dot.y;
+        return this.x - dot.x;
     }
 }
