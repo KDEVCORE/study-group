9182b7215889d9ff37ddbab02e25d6d8c81717d9
KDEVCORE <kdevcore@gmail.com>
2023-02-28 18:35:43 +0900

    challenge delay

--- common challenges/frog 1 (15566)/src/Main.java
+++ common challenges/frog 1 (15566)/src/Main.java
@@ -1,67 +1,39 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-    static ArrayList<LotusInfo>[] lotusList;
-    static boolean[] visited;
+    static int[][] frogTalk, frogPreference, logBridge;
+    static boolean[] lotus;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int lotus = Integer.parseInt(st.nextToken());
-        int log = Integer.parseInt(st.nextToken());
-        FrogInfo[] frogs = new FrogInfo[lotus];
-        for(int i=0; i<lotus; i++) {
+        int node = Integer.parseInt(st.nextToken());
+        int bridge = Integer.parseInt(st.nextToken());
+        lotus = new boolean[node+1];
+        int[][] frogTalk = new int[node][4]; // 대화 주제 흥미도
+        for(int i=0; i<node; i++) {
             st = new StringTokenizer(br.readLine());
-            frogs[i] = new FrogInfo(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), 0, 0);
+            for(int j=0; j<4; j++) frogTalk[i][j] = Integer.parseInt(st.nextToken());
         }
-        for(int i=0; i<lotus; i++) {
+        int[][] frogPreference = new int[node][2]; // 선호 연꽃 번호
+        for(int i=0; i<node; i++) {
             st = new StringTokenizer(br.readLine());
-            frogs[i].preferredA = Integer.parseInt(st.nextToken());
-            frogs[i].preferredB = Integer.parseInt(st.nextToken());
+            for(int j=0; j<2; j++) frogPreference[i][j] = Integer.parseInt(st.nextToken());
         }
-        lotusList = new ArrayList[lotus+1];
-        visited = new boolean[lotus+1];
-        for(int i=1; i<=lotus; i++) lotusList[i] = new ArrayList<>();
-        for(int i=0; i<log; i++) {
+        int[][] log = new int[bridge][3]; // 통나무 다리 정보
+        for(int i=0; i<bridge; i++) {
             st = new StringTokenizer(br.readLine());
-            int nodeA = Integer.parseInt(st.nextToken());
-            int nodeB = Integer.parseInt(st.nextToken());
-            int theme = Integer.parseInt(st.nextToken());
-            lotusList[nodeA].add(new LotusInfo(nodeB, theme));
-            lotusList[nodeB].add(new LotusInfo(nodeA, theme));
+            for(int j=0; j<3; j++) log[i][j] = Integer.parseInt(st.nextToken());
         }
-        for(int i=1; i<=lotus; i++) {
-            if(!visited[i]) dfs(i);
-        }
-        System.out.println();
-    }
-    private static void dfs(int search) {
-        if(visited[search]) return;
-        visited[search] = true;
-        for(int i=1; i<=lotusList.length-1; i++) {
-            lotusList[i].get(i)
-        }
-    }
-}
-class FrogInfo {
-    int food, hobby, family, philosophy, preferredA, preferredB;
-    
-    public FrogInfo(int food, int hobby, int family, int philosophy, int preferredA, int preferredB) {
-        this.food = food;
-        this.hobby = hobby;
-        this.family = family;
-        this.philosophy = philosophy;
-        this.preferredA = preferredA;
-        this.preferredB = preferredB;
-    }
-}
-class LotusInfo {
-    int node, theme;
 
-    public LotusInfo(int node, int theme) {
-        this.node = node;
-        this.theme = theme;
+        for(int i=0; i<node; i++) {
+            for(int j=0; j<2; j++) {
+                dfsNode(frogPreference[i][j]);
+            }
+        }
+    }
+    private static void dfsNode(int n) {
+        if()
     }
 }
