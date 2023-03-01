131b2bd1108d2b44d2afa5c9c6e08d495f09864b
KDEVCORE <kdevcore@gmail.com>
2023-03-01 11:10:14 +0900

    challenge success, analysis

--- common challenges/frog 1 (15566)/src/Main.java
+++ common challenges/frog 1 (15566)/src/Main.java
@@ -8,6 +8,7 @@ public class Main {
     static int[] lotus;
     static int node, bridge;
     static boolean result;
+    static StringBuilder sb;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -30,50 +31,37 @@ public class Main {
             for(int j=0; j<3; j++) logBridge[i][j] = Integer.parseInt(st.nextToken())-1;
         }
         
-        StringBuilder sb = new StringBuilder();
+        sb = new StringBuilder();
         result = false;
         visited = new boolean[node];
         lotus = new int[node];
         dfsNode(0);
-        if(result) {
-            sb.append("YES\n");
-            for(int i=1; i<=lotus.length-1; i++) sb.append(lotus[i] + " ");
-        } else {
-            sb.append("NO\n");
-        }
-        System.out.print(sb);
+        System.out.print(result ? sb : "NO");
     }
     private static void dfsNode(int index) {
         if(index == node) {
-			if(check()) {
-                System.out.println("YES");
-				for (int i = 0; i < node; i++)
-					System.out.print((lotus[i]+1) + " ");
-				System.exit(0);
+            if(validation()) {
                 result = true;
+                sb.append("YES\n");
+                for(int i=0; i<node; i++) sb.append((lotus[i]+1) + " ");
             }
             return;
 		}
-
+        if(result) return;
 		for(int i=0; i<2; i++) {
-			if(frogPreference[index][i] == -1) continue;
-			int num = frogPreference[index][i];
-			if(!visited[num]) {
-				visited[num] = true;
-				lotus[num] = index;
+			int preference = frogPreference[index][i];
+			if(preference == -1) continue;
+			if(!visited[preference]) {
+				visited[preference] = true;
+				lotus[preference] = index;
 				dfsNode(index+1);
-				visited[num] = false;
+				visited[preference] = false;
 			}
 		}
     }
-    private static boolean check() {
+    private static boolean validation() {
 		for (int i=0; i<logBridge.length; i++) {
-			int aNode = logBridge[i][0];
-			int bNode = logBridge[i][1];
-			int theme = logBridge[i][2];
-			int frog1 = lotus[aNode];
-			int frog2 = lotus[bNode];
-			if(frogTalk[frog1][theme] != frogTalk[frog2][theme]) return false;
+			if(frogTalk[lotus[logBridge[i][0]]][logBridge[i][2]] != frogTalk[lotus[logBridge[i][1]]][logBridge[i][2]]) return false;
 		}
 		return true;
 	}
