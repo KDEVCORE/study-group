28d5665c2c54ba5db2003b54aa3caafa74182742
KDEVCORE <kdevcore@gmail.com>
2023-03-10 11:47:46 +0900

    challenge success, refactoring

--- programmers challenges/target number (43165)/src/App.java
+++ programmers challenges/target number (43165)/src/App.java
@@ -18,18 +18,21 @@ public class App {
 }
 
 class Solution {
-    int answer;
+    int[] numbers;
+    int target, answer;
     public int solution(int[] numbers, int target) {
         answer = 0;
-        searchDepth(numbers, target, 0, 0);
+        this.numbers = numbers.clone();
+        this.target = target;
+        searchDepth(0, 0);
         return answer;
     }
-    private void searchDepth(int[] arr, int target, int depth, int count) {
-        if(depth == arr.length-1) {
-            if(count+arr[depth] == target || count-arr[depth] == target) answer++;
+    private void searchDepth(int depth, int count) {
+        if(depth == numbers.length-1) {
+            if(count+numbers[depth] == target || count-numbers[depth] == target) answer++;
             return;
         }
-        searchDepth(arr, target, depth+1, count+arr[depth]);
-        searchDepth(arr, target, depth+1, count-arr[depth]);
+        searchDepth(depth+1, count+numbers[depth]);
+        searchDepth(depth+1, count-numbers[depth]);
     }
 }
